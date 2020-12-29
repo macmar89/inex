e26929c28996619859d9696f3798da772858c5e9
macmar89 <marian.karola@gmail.com>
2020-12-29 20:15:06 +0100

    pridanie react-icons = funkcne, uprava menu, pri velkej obrazovke stale nie je natiahnuty pas cez celu obrazovku

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { Button } from 'react-bootstrap';
 import './Navbar.scss'
 import { Link } from 'react-router-dom'
 import { SidebarData } from '../../data/SidebarData'
+import * as AiIcons from 'react-icons/ai'
+import * as FaIcons from 'react-icons/fa'
 
 
 const NavBar = () => {
@@ -15,16 +16,14 @@ const NavBar = () => {
         <div className='NavbarMenu'>
             <div className='navbar'>
                 <Link to='#' className='menu-bars'>
-                    {/* <FaIcons.FaBars onClick={showSidebar} /> */}
-                    <Button onClick={showSidebar} >Otvor</Button>
+                    <FaIcons.FaBars onClick={showSidebar} />
                 </Link>
             </div>
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className='nav-menu-items' onClick={showSidebar}>
                     <li className='navbar-toggle'>
-                        <Link to='#' className='menu-bars'>
-                            {/* <AiIcons.AiOutlineClose /> */}
-                            <Button>Zavri</Button>
+                        <Link to='#' className='menu-bars-active'>
+                            <AiIcons.AiOutlineClose onClick={showSidebar} />
                         </Link>
                     </li>
                     {SidebarData.map((item, index) => {
@@ -38,7 +37,6 @@ const NavBar = () => {
                     })}
                 </ul>
             </nav>
-            {/* </IconContext.Provider> */}
         </ div >
     );
 }
